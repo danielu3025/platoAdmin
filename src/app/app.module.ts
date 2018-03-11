41883aaa0e5d77ccdd8d960a452b1a769366cc94
danielyosef <danielyosef92@gmail.com>
2018-03-11 19:15:32 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { RightcontentComponent } from './rightcontent/rightcontent.component';
@@ -19,6 +20,12 @@ import { LayoutOptionsComponent } from './rightcontent/nav-options/layout-option
 import { MenuOptionsComponent } from './rightcontent/nav-options/menu-options/menu-options.component';
 import { StockOptionsComponent } from './rightcontent/nav-options/stock-options/stock-options.component';
 import { KitchenOptionsComponent } from './rightcontent/nav-options/kitchen-options/kitchen-options.component';
+import { GroceryFormComponent } from './rightcontent/forms/menu-form/grocery-form/grocery-form.component';
+import { ProductFromComponent } from './rightcontent/forms/menu-form/product-from/product-from.component';
+import { DishFormComponent } from './rightcontent/forms/menu-form/dish-form/dish-form.component';
+import { MealFormComponent } from './rightcontent/forms/menu-form/meal-form/meal-form.component';
+import { FinalMenuFormComponent } from './rightcontent/forms/menu-form/final-menu-form/final-menu-form.component';
+
 
 
 const appRouts: Routes = [
@@ -26,8 +33,14 @@ const appRouts: Routes = [
     { path : 'stock', component: StockComponent },
     { path : 'kitchen', component: KitchenComponent },
     { path : 'layout', component: LayoutComponent },
-    { path : 'menu', component: ManuComponent }
+    { path : 'menu', component: ManuComponent },
+    { path : 'grocery', component: GroceryFormComponent },
+    { path : 'product', component: ProductFromComponent },
+    { path : 'dish', component: DishFormComponent },
+    { path : 'meal', component: MealFormComponent },
+    { path : 'finalMenu', component: FinalMenuFormComponent }
 ] ;
+
 
 @NgModule({
   declarations: [
@@ -46,6 +59,11 @@ const appRouts: Routes = [
     MenuOptionsComponent,
     StockOptionsComponent,
     KitchenOptionsComponent,
+    GroceryFormComponent,
+    ProductFromComponent,
+    DishFormComponent,
+    MealFormComponent,
+    FinalMenuFormComponent
   ],
   imports: [
     BrowserModule,
