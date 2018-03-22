45ab092b04f288378dbdebc6a363b8e230b82987
danielyosef <danielyosef92@gmail.com>
2018-03-22 23:08:25 +0200

    update1 22/3/17

---
+++ src/app/rightcontent/staff/create-staff-form/create-staff-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-staff-form',
+  templateUrl: './create-staff-form.component.html',
+  styleUrls: ['./create-staff-form.component.css']
+})
+export class CreateStaffFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
