85a08bb3cb16e5df3aaa73967b309a4617f2d2d2
danielyosef <danielyosef92@gmail.com>
2018-04-13 13:24:23 +0300

    update 13.4.18

---
+++ src/app/rightcontent/menu/meal.model.ts
@@ -0,0 +1,7 @@
+import {RawMaterial} from "../stock/stock.model";
+export class Grocery{
+    name: string;
+    cookingTime: number;
+    cookingType: string;
+    rawMaterial: RawMaterial[];
+}
--- src/app/rightcontent/tables/table.model.ts
+++ src/app/rightcontent/tables/table.model.ts
@@ -1,4 +1,12 @@
 export class Table{
+    acceabilty: boolean;
     id: string;
+    pBottom: number;
+    pLeft: number;
+    pRight: number;
+    pTop: number;
+    size: number;
+    smoking: boolean;
+    status: string;
     amount: number;
 }
